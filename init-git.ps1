b09cf3a0408ce2b8543ee7c8316a71db12e81200
mattlim-pb <matt@getproductbox.com>
2025-04-11 13:13:53 +0100

    Delete init-git.ps1

--- init-git.ps1
+++
@@ -1,6 +0,0 @@
-cd C:\Users\mlim\Desktop\ml-practice-interviews
-git init
-git add .
-git commit -m "Initial commit for ML Practice Interviews application"
-git remote add origin https://github.com/mattlim-pb/ml-practice-interviews.git
-git push -u origin master
